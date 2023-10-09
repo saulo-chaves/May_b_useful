6b1871301d756bad7ab761904fb386f2f847bad4
Saulo Chaves <101746579+saulo-chaves@users.noreply.github.com>
2023-10-09 20:59:48 -0300

    Update update_asreml.R

--- update_asreml.R
+++ update_asreml.R
@@ -1,4 +1,7 @@
+#' Author: Saulo Chaves (saulo.chaves at ufv.br)
+
 up.mod = function(model){
+  require(asreml)
   repeat{
     if(any(na.exclude(model$vparameters.pc) >= 1)) {
       model = update(model)
